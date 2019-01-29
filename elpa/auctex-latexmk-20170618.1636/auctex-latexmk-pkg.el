05d8e5aefb681e457c1f3d435060796f3071839a
Sparsa Roychowdhury <sparsa@cse.iitb.ac.in>
2019-01-29 16:05:09 +0530

    added auctex-latexmk

---
+++ elpa/auctex-latexmk-20170618.1636/auctex-latexmk-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auctex-latexmk" "20170618.1636" "Add LatexMk support to AUCTeX" '((auctex "11.87")) :commit "4d353522650d7685acbf1d38f7dbc504f734bd84" :keywords '("tex") :authors '(("Tomoya Tanjo" . "ttanjo@gmail.com")) :maintainer '("Tomoya Tanjo" . "ttanjo@gmail.com") :url "https://github.com/tom-tan/auctex-latexmk/")
